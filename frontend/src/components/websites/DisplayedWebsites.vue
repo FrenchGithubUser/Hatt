fd6a2a0116a74751665e858c00b292511856cbba
Leo Moreno <merlin8213@gmail.com>
2023-11-02 16:34:04 -0600

    added toggle all button

--- frontend/src/components/websites/DisplayedWebsites.vue
+++ frontend/src/components/websites/DisplayedWebsites.vue
@@ -1,26 +1,18 @@
 <template>
   <div class="selected-websites shadow-3">
     <div class="websites">
-      <div
-        :class="{
-          website: true,
-          searching: searching && selected,
-          done: doneWebsites.indexOf(website) >= 0 && searching,
-        }"
-        v-for="(selected, website) in selectedWebsites"
-        :key="website"
-      >
+      <div :class="{
+        website: true,
+        searching: searching && selected,
+        done: doneWebsites.indexOf(website) >= 0 && searching,
+      }" v-for="(selected, website) in selectedWebsites" :key="website">
         <q-checkbox v-model="selectedWebsites[website]" class="checkbox" />
         {{ website }}
       </div>
     </div>
-    <q-linear-progress
-      :value="getSearchProgress"
-      size="5px"
-      :animation-speed="500"
-      v-if="searching"
-      key="something"
-    />
+    <q-linear-progress :value="getSearchProgress" size="5px" :animation-speed="500" v-if="searching" key="something" />
+    <!-- Add a button to toggle all checkboxes -->
+    <button @click="toggleAllSelectionBoxes" class="center-button">Toggle All</button>
   </div>
 </template>
 
@@ -55,6 +47,14 @@ export default defineComponent({
       })
       this.selectedWebsites = newSelectedWebsites
     },
+    toggleAllSelectionBoxes() {
+      const allSelected = Object.values(this.selectedWebsites).every(selected => selected);
+      const newSelectedWebsites = {};
+      Object.keys(this.selectedWebsites).forEach(website => {
+        newSelectedWebsites[website] = !allSelected;
+      });
+      this.selectedWebsites = newSelectedWebsites;
+    },
   },
   watch: {
     websites() {
@@ -88,28 +88,45 @@ export default defineComponent({
   border-radius: 15px;
   max-width: 80%;
   margin-top: 10px;
+  padding-bottom: 10px;
   overflow: hidden;
+
   .websites {
     display: flex;
     flex-wrap: wrap;
     justify-content: center;
     padding: 10px;
+
     .website {
       margin-right: 10px;
       padding-right: 7px;
       margin-bottom: 2px;
       margin-top: 2px;
       border-radius: 14px;
+
       &.searching {
         border: solid 2px var(--q-primary);
       }
+
       &.done {
         border: solid 2px $positive;
       }
+
       .checkbox {
         margin-right: -4px;
       }
     }
   }
+}
+
+.center-button {
+  border-radius: 14px;
+  display: block;
+  margin: 0 auto;
+  /* Center the button horizontally */
+  text-align: center;
+  /* Center the button's text horizontally */
+  margin-top: 10px;
+  /* Adjust the top margin as needed */
 }
 </style>
